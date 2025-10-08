598ecb73deba187478e4075b303214cce60dcdec
OKaK52 <a.korotkevich@school.nsu.ru>
2025-10-08 11:03:52 +0700

    new commit <3

---
+++ mainKrotkiy.h
@@ -0,0 +1,13 @@
+//
+// Created by korko on 03.10.2025.
+//
+
+#ifndef FMSH_KROTKIY_MAINKROTKIY_H
+#define FMSH_KROTKIY_MAINKROTKIY_H
+
+
+class mainKrotkiy {
+};
+
+
+#endif //FMSH_KROTKIY_MAINKROTKIY_H
