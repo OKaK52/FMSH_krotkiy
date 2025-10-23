8191c30800b6346fe5d609c3e1171c635d20145e
OKaK52 <a.korotkevich@school.nsu.ru>
2025-10-23 23:17:47 +0700

    new commit <3

--- mainKrotkiy.cpp
+++ mainKrotkiy.cpp
@@ -1,8 +1,51 @@
 #include <iostream>
+#include <string>
+using namespace std;
 
-int main(){
-    int a =2;
-    int b =3;
+// Функция шифрования
+string encrypt(string text, int key) {
+    string result = "";
 
-    std::cout << a+b;
+    for (int i = 0; i < text.length(); i++) {
+        char c = text[i];
+
+        // Шифруем только буквы
+        if (c >= 'A' && c <= 'Z') {
+            // Для больших букв
+            c = (c - 'A' + key) % 26 + 'A';
+        }
+        else if (c >= 'a' && c <= 'z') {
+            // Для маленьких букв
+            c = (c - 'a' + key) % 26 + 'a';
+        }
+
+        result += c;
+    }
+
+    return result;
+}
+
+// Функция расшифровки
+string decrypt(string text, int key) {
+    // Чтобы расшифровать, сдвигаем в обратную сторону
+    return encrypt(text, 26 - key);
+}
+
+int main() {
+    string text;
+    int key;
+
+    cout << "Введите текст: ";
+    getline(cin, text);
+
+    cout << "Введите ключ: ";
+    cin >> key;
+
+    string encrypted = encrypt(text, key);
+    cout << "Зашифровано: " << encrypted << endl;
+
+    string decrypted = decrypt(encrypted, key);
+    cout << "Расшифровано: " << decrypted << endl;
+
+    return 0;
 }
