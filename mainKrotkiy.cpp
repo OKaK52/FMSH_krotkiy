97129c139f9e2307e7ef325a53d552a84cd3c5d5
OKaK52 <a.korotkevich@school.nsu.ru>
2025-10-24 10:48:04 +0700

    new commit <3

--- mainKrotkiy.cpp
+++ mainKrotkiy.cpp
@@ -2,48 +2,13 @@
 #include <string>
 using namespace std;
 
-string encrypt(string text, int key) {
-    string result = "";
-
-    for (int i = 0; i < text.length(); i++) {
-        char ch = text[i];
-
-        if (ch >= 'A' && ch <= 'Z') {
-            ch = ch + key;
-            if (ch > 'Z') ch = ch - 26;
-            if (ch < 'A') ch = ch + 26;
-        }
-        else if (ch >= 'a' && ch <= 'z') {
-            ch = ch + key;
-            if (ch > 'z') ch = ch - 26;
-            if (ch < 'a') ch = ch + 26;
-        }
-
-        result += ch;
-    }
-
-    return result;
-}
-
-string decrypt(string text, int key) {
-    return encrypt(text, -key);
-}
-
 int main() {
-    string text;
-    int key;
+    string text = "Hello";
+    string key = "key";
 
-    cout << "Tipe text: ";
-    getline(cin, text);
+    cout << "First text: " << text << endl;
+    cout << "Key: " << key << endl;
 
-    cout << "Tipe key: ";
-    cin >> key;
-
-    string encrypted = encrypt(text, key);
-    cout << "Encrypted: " << encrypted << endl;
-
-    string decrypted = decrypt(encrypted, key);
-    cout << "Decrypted: " << decrypted << endl;
 
     return 0;
 }
